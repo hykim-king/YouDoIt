6a21ff9fb4fe6a2b2b17a265b8208f6095d4dde6
tnals <tnals5035@gmail.com>
2026-04-23 17:42:58 +0900

    패키지 파일 2개, 자바파일 2개, .project파일(환경) 추가

---
+++ src/kiosk/menu/cmn/MenuDto.java
@@ -0,0 +1,14 @@
+/**
+ * 파일명: DTO.java <br>
+ * 설 명: 모든 Value Object의 Parent <br>
+ * 작성자: user <br>
+ * 생성일: 2026-04-23 <br>
+ */
+package kiosk.menu.cmn;
+
+/**
+ * Data Transfer Object, 데이터 전송 객체
+ */
+public class MenuDto {
+
+}
--- src/kiosk/menu/domain/MenuVO.java
+++ src/kiosk/menu/domain/MenuVO.java
@@ -1,5 +1,8 @@
 package kiosk.menu.domain;
 
 public class MenuVO {
+	
+	private String food;//메뉴명
+	private int price;	//가격
 
 }
